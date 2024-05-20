49c2593fb103a9e07f9f91b34566e6276b59580a
naterex <nathanieljrex@gmail.com>
2024-05-20 15:10:32 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# malmo-ext
-A wrapper for Microsoft's [Malmo Platform](https://github.com/microsoft/malmo/tree/master) for creating automated agent action sequences.
+# malmo_ext
+
+A wrapper for Microsoft's [Malmo Platform](https://github.com/microsoft/malmo/tree/master) for creating automated agent action sequences in [Minecraft](https://www.minecraft.net/en-us).
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='malmo_ext',
+      version='0.0.1',
+      description='A wrapper for Microsoft\'s Malmo Platform for creating automated agent action sequences in Minecraft.',
+      url='https://github.com/NateRex/malmo_ext',
+      author='Nate Rex',
+      license='MIT',
+      packages=['malmo_ext'],
+      zip_safe=False)
